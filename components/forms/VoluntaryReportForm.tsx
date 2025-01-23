af1710754d82bbedd819454b1b42d8e115d372ed
aaguinagalde <aaguinagalde@transmandu.com>
2025-01-23 16:27:53 -0400

    se agrego el formulario para reporte voluntario2

---
+++ components/forms/VoluntaryReportForm.tsx
@@ -0,0 +1,327 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Separator } from "../ui/separator";
+import { Textarea } from "../ui/textarea";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "../ui/calendar";
+import { cn } from "@/lib/utils";
+import { es } from "date-fns/locale";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { useState } from "react";
+
+const FormSchema = z.object({
+  identification_date: z
+    .date()
+    .refine((val) => !isNaN(val.getTime()), { message: "Invalid Date" }),
+  report_date: z
+    .date()
+    .refine((val) => !isNaN(val.getTime()), { message: "Invalid Date" }),
+
+  selectedArea: z.enum([
+    "Operacional",
+    "Mantenimiento",
+    "Administracion y RRHH",
+    "Control de calidad",
+    "Tecnologia e informacion",
+  ]),
+  danger_place: z.string(),
+  description: z.string(),
+  consequences: z.string(),
+  name: z.string().optional(),
+  last_name: z.string().optional(),
+  phone: z.string().optional(),
+  email: z.string().email().optional(),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+interface FormProps {
+  onClose: () => void;
+}
+// { onClose }: FormProps
+// lo de arriba va en prop
+export function VoluntaryReportForm() {
+  const [showOptionalFields, setShowOptionalFields] = useState(false);
+
+  const options = [
+    { value: "Operacional" },
+    { value: "Mantenimiento" },
+    { value: "Administracion y RRHH" },
+    { value: "Control de calidad" },
+    { value: "Tecnologia e informacion" },
+  ];
+
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      report_date: new Date(),
+      identification_date: new Date(),
+
+      danger_place: "",
+      description: "",
+      consequences: "",
+      name: "",
+      last_name: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = (data: FormSchemaType) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-3"
+      >
+        <FormField
+          control={form.control}
+          name="identification_date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col mt-2.5">
+              <FormLabel>Fecha de identificacion de peligro</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP", {
+                          locale: es,
+                        })
+                      ) : (
+                        <span>Seleccione una fecha...</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                    locale={es}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="report_date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col mt-2.5">
+              <FormLabel>Fecha del reporte</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP", {
+                          locale: es,
+                        })
+                      ) : (
+                        <span>Seleccione una fecha...</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                    locale={es}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="selectedArea"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Seleccionar el area </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Area de iedntificacion" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectGroup>
+                    {options.map((option) => (
+                      <SelectItem key={option.value} value={option.value}>
+                        {option.value}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="danger_place"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Lugar de identificacion de peligro</FormLabel>
+              <FormControl>
+                <Input placeholder="ej: Hangar13B" {...field} />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Descripcion del peligro</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Breve descripcion del peligro identificado"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showOptionalFields}
+              onChange={() => setShowOptionalFields(!showOptionalFields)}
+              className="mr-2"
+            />
+            ¿Desea realizar su reporte de forma anonima?
+          </label>
+        </div>
+        {!showOptionalFields && (
+          <>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Nombre de quien reporta" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="last_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Apellido</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Apellido de quien reporta" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Telefono</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Telefono de contacto " {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Correo electronico</FormLabel>
+                  <FormControl>
+                    <Input placeholder="ejemplo@transmandu.com" {...field} />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+          </>
+        )}
+        <div className="flex justify-between items-center gap-x-4">
+          <Separator className="flex-1" />
+          <p className="text-muted-foreground">SIGEAC</p>
+          <Separator className="flex-1" />
+        </div>
+        <Button>Enviar reporte</Button>
+      </form>
+    </Form>
+  );
+}
