44f6ff9fa241fe499dca955fe5f0386f31cdff6b
aaguinagalde <aaguinagalde@transmandu.com>
2025-01-30 13:19:24 -0400

    cambio de nombre para que sea Create como el resto

--- components/dialogs/CreateObligatoryDialog.tsx
+++ components/dialogs/CreateObligatoryDialog.tsx
@@ -10,7 +10,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { ObligatoryReportForm } from "../forms/ObligatoryReporForm";
+import { ObligatoryReportForm } from "../forms/CreateObligatoryReportForm";
 
 export function CreateObligatoryDialog() {
   return (
