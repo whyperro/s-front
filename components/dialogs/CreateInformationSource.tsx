93de7c95875c7f933054fb23096a974ba687dc2f
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-04 16:52:58 -0400

    dialogo para formulario de fuente de informacion 2

---
+++ components/dialogs/CreateInformationSource.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { VoluntaryReportForm } from "@/components/forms/CreateVoluntaryReportForm";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { InformationSource } from "../forms/CreateInformationSourceForm";
+
+export default function CreateInformationSourceDialog() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Card className="flex">
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button
+              onClick={() => setOpen(true)}
+              variant="outline"
+              size="sm"
+              className=" hidden h-8 lg:flex"
+            >
+              Nuevo reporte
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="flex flex-col max-w-2xl m-2">
+            <DialogHeader>
+              <DialogTitle></DialogTitle>
+              <DialogDescription></DialogDescription>
+            </DialogHeader>
+            <InformationSource onClose={() => setOpen(false)} />
+          </DialogContent>
+        </Dialog>
+      </Card>
+    </>
+  );
+}
