4291d15ddc58c41164469a457ed0d600793ea13c
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-04 16:55:00 -0400

    formulario para fuente de informacion creado

---
+++ components/forms/CreateInformationSourceForm.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { format } from "date-fns";
+import { CalendarIcon, Loader2 } from "lucide-react";
+import { es } from "date-fns/locale";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { Calendar } from "@/components/ui/calendar";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "../ui/label";
+import { useCreateVoluntaryReport } from "@/actions/sms/reporte_voluntario/actions";
+import { useCreateInformationSource } from "@/actions/sms/tipos_fuente/actions";
+
+const FormSchema = z.object({
+  type: z.string(),
+  name: z.string(),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+interface FormProps {
+  onClose: () => void;
+}
+// { onClose }: FormProps
+// lo de arriba va en prop
+export function InformationSourceForm({ onClose }: FormProps) {
+  const { createInformationSource } = useCreateInformationSource();
+
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {},
+  });
+
+  const onSubmit = async (data: FormSchemaType) => {
+    //await createVoluntaryReport.mutateAsync(data);
+    onClose();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-3"
+      >
+        <FormLabel className="text-lg text-center">
+          Formulario de identificación de peligro
+        </FormLabel>
+        <div className="flex gap-2 items-center justify-center  ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre de la Fuente</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Seleccionar Tipo</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Seleccionar Tipo" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="PROACTIVO">PROACTIVO</SelectItem>
+                    <SelectItem value="REACTIVO">REACTIVO</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex justify-between items-center gap-x-4">
+          <Separator className="flex-1" />
+          <p className="text-muted-foreground">SIGEAC</p>
+          <Separator className="flex-1" />
+        </div>
+        <Button disabled={createInformationSource.isPending}>
+          {createInformationSource.isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            "Enviar Reporte"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+}
