e9ed17eac917c2cbee07dd90541d0cad277ec2ce
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-04 16:54:33 -0400

    accion para traer tipos de fuente

---
+++ actions/sms/tipos_fuente/actions.ts
@@ -0,0 +1,43 @@
+import axiosInstance from "@/lib/axios";
+import {
+  ComponentArticle,
+  ConsumableArticle,
+  DispatchRequest,
+  Request,
+} from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+interface InformationSourceData {
+  name: string;
+  type: string;
+}
+
+export const useCreateInformationSource = () => {
+  const queryClient = useQueryClient();
+  const createMutation = useMutation({
+    mutationKey: ["information-sources"],
+    mutationFn: async (data: InformationSourceData) => {
+      await axiosInstance.post("/transmandu/information-sources", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["information-sources"] });
+      toast.success("¡Creado!", {
+        description: ` La fuente voluntario ha sido creado correctamente.`,
+      });
+    },
+    onError: (error) => {
+      toast.error("Oops!", {
+        description: "No se pudo crear el reporte...",
+      });
+      console.log(error);
+    },
+  });
+  return {
+    createInformationSource: createMutation,
+  };
+};
