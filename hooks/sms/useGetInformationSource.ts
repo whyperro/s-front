9f164542c77bc4c97b6e231bfb3d7543c8482302
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-05 13:47:20 -0400

    arreglada la key

--- hooks/sms/useGetInformationSource.ts
+++ hooks/sms/useGetInformationSource.ts
@@ -2,15 +2,15 @@ import axiosInstance from "@/lib/axios";
 import { InformationSource, VoluntaryReport } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchInformationSource = async (): Promise<InformationSource[]> => {
-  const { data } = await axiosInstance.get("/transmandu/voluntary-reports");
+const fetchInformationSources = async (): Promise<InformationSource[]> => {
+  const { data } = await axiosInstance.get("/transmandu/information-sources");
   return data;
 };
 
-export const useGetVoluntaryReports = () => {
+export const useGetInformationSources = () => {
   return useQuery<InformationSource[]>({
-    queryKey: ["iformation-source"],
-    queryFn: fetchInformationSource,
+    queryKey: ["information-sources"],
+    queryFn: fetchInformationSources,
     staleTime: 1000 * 60 * 5, // 5 minutos
   });
 };
