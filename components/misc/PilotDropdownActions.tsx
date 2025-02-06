9d8290eca9d1f14a109c17ea7a6b25e5d3e5ea29
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-06 15:25:52 -0400

    se creo el menu desplegable para tabla piloto

---
+++ components/misc/PilotDropdownActions.tsx
@@ -0,0 +1,101 @@
+import { useDeletePilot } from "@/actions/sms/piloto/actions";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Pilot } from "@/types";
+import { EyeIcon, Loader2, MoreHorizontal, Trash2 } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { useRouter } from "next/navigation";
+
+const PilotDropdownActions = ({ pilot }: { pilot: Pilot }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const { deletePilot } = useDeletePilot();
+
+  const router = useRouter();
+
+  const handleDelete = async (id: number | string) => {
+    await deletePilot.mutateAsync(id);
+    setOpen(false);
+  };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Abrir menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+          align="center"
+          className="flex gap-2 justify-center"
+        >
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Trash2 className="size-5 text-red-500" />
+            </DropdownMenuItem>
+          </DialogTrigger>
+
+          <DropdownMenuItem
+            onClick={() => {
+              router.push(`/ajustes/globales/pilotos/${pilot.dni}`);
+            }}
+          >
+            <EyeIcon className="size-5" />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-center">
+            ¿Seguro que desea eliminar el piloto?
+          </DialogTitle>
+          <DialogDescription className="text-center p-2 mb-0 pb-0">
+            Esta acción es irreversible y estaría eliminando por completo el
+            piloto seleccionado.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="flex flex-col-reverse gap-2 md:gap-0">
+          <Button
+            className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
+            onClick={() => setOpen(false)}
+            type="submit"
+          >
+            Cancelar
+          </Button>
+
+          <Button
+            disabled={deletePilot.isPending}
+            className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
+            onClick={() => handleDelete(pilot.id)}
+          >
+            {deletePilot.isPending ? (
+              <Loader2 className="size-4 animate-spin" />
+            ) : (
+              <p>Confirmar</p>
+            )}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PilotDropdownActions;
