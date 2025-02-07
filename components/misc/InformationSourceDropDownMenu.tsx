de345cf1cfb4d7b13b04814a7e395af83e5cb9a5
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-07 16:36:36 -0400

    Se agrego la opcion de editar al menu, + funcionamiento

--- components/misc/InformationSourceDropDownMenu.tsx
+++ components/misc/InformationSourceDropDownMenu.tsx
@@ -6,7 +6,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { InformationSource, Pilot } from "@/types";
-import { EyeIcon, Loader2, MoreHorizontal, Trash2 } from "lucide-react";
+import {
+  ClipboardPen,
+  EyeIcon,
+  Loader2,
+  MoreHorizontal,
+  Trash2,
+} from "lucide-react";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import {
@@ -20,13 +26,15 @@ import {
 } from "../ui/dialog";
 import { useRouter } from "next/navigation";
 import { useDeleteInformationSource } from "@/actions/sms/tipos_fuente/actions";
+import { EditInformationSourceForm } from "../forms/EditInformationSourceForm";
 
 const InformationSourceDropdownActions = ({
   informationSource,
 }: {
   informationSource: InformationSource;
 }) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+
   const [openEdit, setOpenEdit] = useState<boolean>(false);
 
   const { deleteInformationSource } = useDeleteInformationSource();
@@ -35,10 +43,10 @@ const InformationSourceDropdownActions = ({
 
   const handleDelete = async (id: number | string) => {
     await deleteInformationSource.mutateAsync(id);
-    setOpen(false);
+    setOpenDelete(false);
   };
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <Button variant="ghost" className="h-8 w-8 p-0">
@@ -51,55 +59,71 @@ const InformationSourceDropdownActions = ({
           align="center"
           className="flex gap-2 justify-center"
         >
-          <DialogTrigger asChild>
-            <DropdownMenuItem>
-              <Trash2 className="size-5 text-red-500" />
-            </DropdownMenuItem>
-          </DialogTrigger>
-          <DropdownMenuItem
-            onClick={() => {
-              router.push(`/ajustes/globales/pilotos/${informationSource.id}`);
-            }}
-          >
-            <EyeIcon className="size-5" />
+          <DropdownMenuItem onClick={() => setOpenEdit(true)}>
+            <ClipboardPen className="size-5" />
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpenDelete(true)}>
+            <Trash2 className="size-5 text-red-500" />
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
 
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-center">
-            ¿Seguro que desea eliminar la uente?
-          </DialogTitle>
-          <DialogDescription className="text-center p-2 mb-0 pb-0">
-            Esta acción es irreversible y estaría eliminando por completo el
-            piloto seleccionado.
-          </DialogDescription>
-        </DialogHeader>
+      {/* DIALOGO DE ELIMINAR */}
+      <Dialog open={openDelete} onOpenChange={setOpenDelete}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-center">
+              ¿Seguro que desea eliminar la fuente?
+            </DialogTitle>
+            <DialogDescription className="text-center p-2 mb-0 pb-0">
+              Esta acción es irreversible y estaría eliminando por completo el
+              piloto seleccionado.
+            </DialogDescription>
+          </DialogHeader>
 
-        <DialogFooter className="flex flex-col-reverse gap-2 md:gap-0">
-          <Button
-            className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
-            onClick={() => setOpen(false)}
-            type="submit"
-          >
-            Cancelar
-          </Button>
+          <DialogFooter className="flex flex-col-reverse gap-2 md:gap-0">
+            <Button
+              className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
+              onClick={() => setOpenDelete(false)}
+              type="submit"
+            >
+              Cancelar
+            </Button>
 
-          <Button
-            disabled={deleteInformationSource.isPending}
-            className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
-            onClick={() => handleDelete(informationSource.id)}
-          >
-            {deleteInformationSource.isPending ? (
-              <Loader2 className="size-4 animate-spin" />
-            ) : (
-              <p>Confirmar</p>
-            )}
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+            <Button
+              disabled={deleteInformationSource.isPending}
+              className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
+              onClick={() => handleDelete(informationSource.id)}
+            >
+              {deleteInformationSource.isPending ? (
+                <Loader2 className="size-4 animate-spin" />
+              ) : (
+                <p>Confirmar</p>
+              )}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      {/* DIALOGO DE EDITAR */}
+      <Dialog open={openEdit} onOpenChange={setOpenEdit}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-center">Edicion de Fuente</DialogTitle>
+            <DialogDescription className="text-center p-2 mb-0 pb-0">
+              Edicion de fuentes.
+            </DialogDescription>
+            PONES AQUI TU FORMULARIO AL QUE LE VAS A PASAR COMO
+            initialData=informationSource
+
+            <EditInformationSourceForm
+              initialData={informationSource}
+              onClose={() => setOpenEdit(false)}
+            />
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
