582cb6dcb2e7584678cc850fcb3e3e84e05d0d89
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-10 15:57:19 -0400

    se actualizaron nombres de variables en columnas

--- app/(companies)/transmandu/sms/reportes_obligatorios/columns.tsx
+++ app/(companies)/transmandu/sms/reportes_obligatorios/columns.tsx
@@ -5,12 +5,12 @@ import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/tables/DataTableHeader";
 
 import { Checkbox } from "@/components/ui/checkbox";
-import { MandatoryReport } from "@/types";
+import { ObligatoryReport } from "@/types";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import Link from "next/link";
 
-export const columns: ColumnDef<MandatoryReport>[] = [
+export const columns: ColumnDef<ObligatoryReport>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -34,21 +34,14 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "report_number",
+    accessorKey: "report_code",
     header: ({ column }) => (
       <DataTableColumnHeader filter column={column} title="Nro. de Reporte" />
     ),
     meta: { title: "Nro. de Reporte" },
     cell: ({ row }) => {
       return (
-        <div className="flex justify-center">
-          <Link
-            href={`/hangar74/compras/cotizaciones/${row.original.report_number}`}
-            className="font-bold text-center hover:italic hover:scale-110 transition-all"
-          >
-            {row.original.report_number}
-          </Link>
-        </div>
+        <div className="flex justify-center">{row.original.report_code}</div>
       );
     },
   },
@@ -75,7 +68,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     cell: ({ row }) => {
       return (
         <p className="font-medium text-center">
-          {format(row.original.report_date, "PPP", {
+          {format(row.original.incident_date, "PPP", {
             locale: es,
           })}
         </p>
@@ -90,22 +83,9 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     cell: ({ row }) => {
       return (
         <p className="font-medium text-center">
-          {format(row.original.report_date, "HH:mm", {
+          {format(row.original.incident_date, "HH:mm", {
             locale: es,
           })}
-        </p>
-      );
-    },
-  },
-  {
-    accessorKey: "incident_location",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Lugar del suceso" />
-    ),
-    cell: ({ row }) => {
-      return (
-        <p className="font-medium text-center">
-          {row.original.incident_location}
         </p>
       );
     },
@@ -116,16 +96,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
       <DataTableColumnHeader column={column} title="Datos del piloto" />
     ),
     cell: ({ row }) => {
-      return (
-        <p className="font-medium text-center">
-          {row.original.pilot.id}
-          {row.original.pilot.name}
-          {row.original.pilot.last_name}
-          {row.original.pilot.license_number}
-          {row.original.pilot.phone}
-          {row.original.pilot.email}
-        </p>
-      );
+      return <p className="font-medium text-center"></p>;
     },
   },
   {
@@ -134,15 +105,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
       <DataTableColumnHeader column={column} title="Datos del copiloto" />
     ),
     cell: ({ row }) => {
-      return (
-        <p className="font-medium text-center">
-          {row.original.copilot.id}
-          {row.original.copilot.name}
-          {row.original.copilot.last_name}
-          {row.original.copilot.phone}
-          {row.original.copilot.email}
-        </p>
-      );
+      return <p className="font-medium text-center"></p>;
     },
   },
   {
@@ -153,7 +116,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     cell: ({ row }) => {
       return (
         <p className="font-medium text-center">
-          {format(row.original.report_date, "HH:mm", {
+          {format(row.original.flight_time, "HH:mm", {
             locale: es,
           })}
         </p>
@@ -161,7 +124,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     },
   },
   {
-    accessorKey: "aircraft",
+    accessorKey: "aircraft_acronym",
     header: ({ column }) => (
       <DataTableColumnHeader
         column={column}
@@ -171,7 +134,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     cell: ({ row }) => {
       return (
         <p className="font-medium text-center">
-          {row.original.aircraft.acronym}
+          {row.original.aircraft_acronym}
         </p>
       );
     },
@@ -195,10 +158,8 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     ),
     cell: ({ row }) => {
       return (
-        <p className="font-medium text-center">
-          {format(row.original.incident_date, "PPP", {
-            locale: es,
-          })}
+        <p className="flex justify-center text-muted-foreground italic">
+          {row.original.aircraft_model}
         </p>
       );
     },
@@ -219,7 +180,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     ),
     cell: ({ row }) => (
       <div className="flex justify-center">
-        {row.original.flight_destination}
+        {row.original.flight_destiny}
       </div>
     ),
   },
@@ -233,7 +194,7 @@ export const columns: ColumnDef<MandatoryReport>[] = [
     ),
     cell: ({ row }) => (
       <div className="flex justify-center">
-        {row.original.alternate_destination}
+        {row.original.flight_alt_destiny}
       </div>
     ),
   },
