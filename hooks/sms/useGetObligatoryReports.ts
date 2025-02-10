7c1aab708351b5f71a66449b91cbdf78f60bd288
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-10 15:57:42 -0400

    accion para  obtener reportes obligatorios creado

---
+++ hooks/sms/useGetObligatoryReports.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "@/lib/axios";
+import { ObligatoryReport } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchObligatoryReports = async (): Promise<ObligatoryReport[]> => {
+  const { data } = await axiosInstance.get("/transmandu/obligatory-reports");
+  return data;
+};
+
+export const useGetObligatoryReports = () => {
+  return useQuery<ObligatoryReport[]>({
+    queryKey: ["obligatory-reports"],
+    queryFn: fetchObligatoryReports,
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+};
