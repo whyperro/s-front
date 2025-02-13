6330464ab0e34b19f05a51fb13c2b6868d95be19
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-13 14:21:14 -0400

    creada la accion para traer los identificacines peligro

--- hooks/sms/useGetDangerIdentification.ts
+++ hooks/sms/useGetDangerIdentification.ts
@@ -1,16 +1,16 @@
 import axiosInstance from "@/lib/axios";
-import { InformationSource, VoluntaryReport } from "@/types";
+import { DangerIdentification, InformationSource, VoluntaryReport } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchInformationSources = async (): Promise<InformationSource[]> => {
-  const { data } = await axiosInstance.get("/transmandu/information-sources");
+const fetchDangerIdentifications = async (): Promise<DangerIdentification[]> => {
+  const { data } = await axiosInstance.get("/transmandu/danger-identifications");
   return data;
 };
 
-export const useGetInformationSources = () => {
-  return useQuery<InformationSource[]>({
-    queryKey: ["information-sources"],
-    queryFn: fetchInformationSources,
+export const useGetDangerIdentifications = () => {
+  return useQuery<DangerIdentification[]>({
+    queryKey: ["danger-identifications"],
+    queryFn: fetchDangerIdentifications,
     staleTime: 1000 * 60 * 5, // 5 minutos
   });
 };
