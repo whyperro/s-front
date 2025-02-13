405f7726077733cdc95326f81943dfd3aec59195
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-13 14:23:15 -0400

    agredado al menu opcion para crear identificacion a patir de un reporte

--- components/misc/VoluntaryReportDropDownMenu.tsx
+++ components/misc/VoluntaryReportDropDownMenu.tsx
@@ -6,7 +6,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { InformationSource, Pilot, VoluntaryReport } from "@/types";
-import { EyeIcon, Loader2, MoreHorizontal, Trash2 , ClipboardPenLine } from "lucide-react";
+import {
+  EyeIcon,
+  Loader2,
+  MoreHorizontal,
+  Trash2,
+  ClipboardPenLine,
+} from "lucide-react";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import {
@@ -21,6 +27,8 @@ import {
 import { useRouter } from "next/navigation";
 import { useDeleteInformationSource } from "@/actions/sms/tipos_fuente/actions";
 import { useDeleteVoluntaryReport } from "@/actions/sms/reporte_voluntario/actions";
+import CreateDangerIdentificationForm from "../forms/CreateIdentificationForm";
+import CreateDangerIdentificationDialog from "../dialogs/CreateDangerIdentificationDialog";
 
 const VoluntaryReportDropdownActions = ({
   voluntaryReport,
@@ -32,6 +40,10 @@ const VoluntaryReportDropdownActions = ({
 
   const { deleteVoluntaryReport } = useDeleteVoluntaryReport();
 
+  const [openCreateDangerIdentification, setOpenCreateDangerIdentification] =
+    useState<boolean>(false);
+
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
   const router = useRouter();
 
   const handleDelete = async (id: number | string) => {
@@ -39,80 +51,89 @@ const VoluntaryReportDropdownActions = ({
     setOpen(false);
   };
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Abrir menu</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
 
-        <DropdownMenuContent
-          align="center"
-          className="flex gap-2 justify-center"
-        >
-          <DialogTrigger asChild>
+          <DropdownMenuContent
+            align="center"
+            className="flex gap-2 justify-center"
+          >
+            <DialogTrigger asChild>
+              <DropdownMenuItem onClick={() => setOpenDelete(true)}>
+                <Trash2 className="size-5 text-red-500" />
+              </DropdownMenuItem>
+            </DialogTrigger>
 
-            <DropdownMenuItem>
-              <Trash2 className="size-5 text-red-500" />
+            <DropdownMenuItem
+              onClick={() => {
+                router.push(`/ajustes/globales/pilotos/${voluntaryReport.id}`);
+              }}
+            >
+              <EyeIcon className="size-5" />
             </DropdownMenuItem>
-          </DialogTrigger>
 
-          <DropdownMenuItem
-            onClick={() => {
-              router.push(`/ajustes/globales/pilotos/${voluntaryReport.id}`);
-            }}
-          >
-            <EyeIcon className="size-5" />
-          </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setOpenCreateDangerIdentification(true)}
+            >
+              <ClipboardPenLine className="size-5" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
-          <DropdownMenuItem
-            onClick={() => {
-              router.push(`/ajustes/globales/pilotos/${voluntaryReport.id}`);
-            }}
-          >
-            <ClipboardPenLine className="size-5" />
-          </DropdownMenuItem>
-            
-            
-        </DropdownMenuContent>
-      </DropdownMenu>
+        <Dialog open={openDelete} onOpenChange={setOpenDelete}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="text-center">
+                ¿Seguro que desea eliminar el reporte??
+              </DialogTitle>
+              <DialogDescription className="text-center p-2 mb-0 pb-0">
+                Esta acción es irreversible y estaría eliminando por completo el
+                reporte seleccionado.
+              </DialogDescription>
+            </DialogHeader>
 
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-center">
-            ¿Seguro que desea eliminar el reporte?
-          </DialogTitle>
-          <DialogDescription className="text-center p-2 mb-0 pb-0">
-            Esta acción es irreversible y estaría eliminando por completo el
-            reporte seleccionado.
-          </DialogDescription>
-        </DialogHeader>
+            <DialogFooter className="flex flex-col-reverse gap-2 md:gap-0">
+              <Button
+                className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
+                onClick={() => setOpenDelete(false)}
+                type="submit"
+              >
+                Cancelar
+              </Button>
 
-        <DialogFooter className="flex flex-col-reverse gap-2 md:gap-0">
-          <Button
-            className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
-            onClick={() => setOpen(false)}
-            type="submit"
-          >
-            Cancelar
-          </Button>
+              <Button
+                disabled={deleteVoluntaryReport.isPending}
+                className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
+                onClick={() => handleDelete(voluntaryReport.id)}
+              >
+                {deleteVoluntaryReport.isPending ? (
+                  <Loader2 className="size-4 animate-spin" />
+                ) : (
+                  <p>Confirmar</p>
+                )}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
 
-          <Button
-            disabled={deleteVoluntaryReport.isPending}
-            className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
-            onClick={() => handleDelete(voluntaryReport.id)}
-          >
-            {deleteVoluntaryReport.isPending ? (
-              <Loader2 className="size-4 animate-spin" />
-            ) : (
-              <p>Confirmar</p>
-            )}
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        <Dialog open={openCreateDangerIdentification} onOpenChange={setOpenCreateDangerIdentification}>
+          <DialogContent className="flex flex-col max-w-2xl m-2">
+            <DialogHeader>
+              <DialogTitle></DialogTitle>
+              <DialogDescription></DialogDescription>
+            </DialogHeader>
+            <CreateDangerIdentificationForm onClose={() => setOpenCreateDangerIdentification(false)} />
+          </DialogContent>
+        </Dialog>
+      </Dialog>
+    </>
   );
 };
 
