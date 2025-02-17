debfad935fc76df767be370f4fc94d76aed92f9f
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-17 16:42:05 -0400

    crenado tabla para plan de mitigacion y analisis

---
+++ app/(companies)/transmandu/sms/planes_de_mitigacion/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ContentLayout } from "@/components/layout/ContentLayout";
+import { useGetQuotes } from "@/hooks/compras/useGetQuotes";
+import { useCompanyStore } from "@/stores/CompanyStore";
+import { Loader2 } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
+const MitigationPlanPage = () => {
+  const { selectedStation, selectedCompany } = useCompanyStore();
+  const {
+    data: reportes,
+    isLoading,
+    isError,
+  } = useGetQuotes(
+    (selectedCompany && selectedCompany.split(" ").join("")) || null,
+    selectedStation || null
+  );
+
+
+  
+  return (
+    <ContentLayout title="Planes de Mitigacion">
+      <div className="flex flex-col gap-y-2">
+        {isLoading && (
+          <div className="flex w-full h-full justify-center items-center">
+            <Loader2 className="size-24 animate-spin mt-48" />
+          </div>
+        )}
+        {reportes && <DataTable columns={columns} data={[]} />}
+        {isError && (
+          <p className="text-sm text-muted-foreground">
+            Ha ocurrido un error al cargar los planes de mitigaction...
+          </p>
+        )}
+      </div>
+    </ContentLayout>
+  );
+};
+
+export default MitigationPlanPage;
