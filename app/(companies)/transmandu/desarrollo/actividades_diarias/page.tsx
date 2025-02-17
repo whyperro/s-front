10f585fee80688a90c89f60f4e1777b19f05f212
Henry Guzman <hguzman@transmandu.com>
2025-02-17 13:18:38 -0400

    routing to Transmandu Desarrollo Module

---
+++ app/(companies)/transmandu/desarrollo/actividades_diarias/page.tsx
@@ -0,0 +1,10 @@
+import { ContentLayout } from '@/components/layout/ContentLayout'
+import React from 'react'
+
+const DailyActivities = () => {
+  return (
+    <ContentLayout title='Actividades Diarias'>ActividadesDiarias</ContentLayout>
+  )
+}
+
+export default DailyActivities
