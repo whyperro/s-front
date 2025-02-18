15e673124b34cd64ba148877c7cbaba953f314fc
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-18 16:48:41 -0400

    agragando datos a la tabla

--- app/(companies)/transmandu/sms/planes_de_mitigacion/columns.tsx
+++ app/(companies)/transmandu/sms/planes_de_mitigacion/columns.tsx
@@ -5,10 +5,11 @@ import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/tables/DataTableHeader";
 
 import { Checkbox } from "@/components/ui/checkbox";
-import { MitigationPlan } from "@/types";
 import Link from "next/link";
+import { MitigationTable } from "@/types";
+import MitigationPlanPage from "./page";
 
-export const columns: ColumnDef<MitigationPlan>[] = [
+export const columns: ColumnDef<MitigationTable>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -32,16 +33,39 @@ export const columns: ColumnDef<MitigationPlan>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "id",
+    accessorKey: "analysis",
     header: ({ column }) => (
-      <DataTableColumnHeader filter column={column} title="Nro. de Reporte" />
+      <DataTableColumnHeader filter column={column} title="Analisis" />
     ),
-    meta: { title: "Nro. de Plan de mitigacion" },
+    meta: { title: "Analisis" },
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-col justify-center">
+          <hr />
+          <p>Probabilidad: {row.original.analysis.probability}</p>
+          <hr />
+          <p>Severidad: {row.original.analysis.severity}</p>
+          <hr />
+          <p>Resultado: {row.original.analysis.result}</p>
+          <hr />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "mitigation_plan",
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        filter
+        column={column}
+        title="Nro. Plan de Mitigacion"
+      />
+    ),
+    meta: { title: "Nro. de Plan de Mitigacion" },
     cell: ({ row }) => {
       return (
         <div className="flex justify-center">
-
-            {row.original.id}
+          {row.original.mitigation_plan?.id}
         </div>
       );
     },
@@ -50,22 +74,48 @@ export const columns: ColumnDef<MitigationPlan>[] = [
   {
     accessorKey: "description",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Lugar del suceso" />
+      <DataTableColumnHeader column={column} title="Descripcion del Plan" />
     ),
+    meta: { title: "Descripcion" },
     cell: ({ row }) => {
       return (
-        <p className="font-medium text-center">{row.original.description}</p>
+        <p className="font-medium text-center">
+          {row.original.mitigation_plan?.description}
+        </p>
       );
     },
   },
   {
     accessorKey: "measures",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Datos del piloto" />
+      <DataTableColumnHeader column={column} title="Agregar Medidas" />
     ),
     cell: ({ row }) => {
-      const measure = row.original.measures;
+      // const measure = row.original.measures;
       return <></>;
+    },
+  },
+  {
+    // Debo agregar N/A no aplica, en caso de que esten vacios los campos .
+    accessorKey: "mitigation_plan",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Analisis-Post Mitigacion" />
+    ),
+    cell: ({ row }) => {
+      const mitigation_analysis = row.original.mitigation_plan?.analysis;
+      return (
+        <div className="font-medium text-center">
+          <div className="flex flex-col justify-center">
+            <hr />
+            <p>Probabilidad: {mitigation_analysis?.probability}</p>
+            <hr />
+            <p>Severidad: {mitigation_analysis?.severity}</p>
+            <hr />
+            <p>Resultado: {mitigation_analysis?.result}</p>
+            <hr />
+          </div>
+        </div>
+      );
     },
   },
 ];
