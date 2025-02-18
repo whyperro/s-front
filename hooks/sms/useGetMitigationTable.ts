ca3ab7668a5f0094f9219a0f1a6a6ca5b7a852eb
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-18 16:46:59 -0400

    hook para datos de la tabla

---
+++ hooks/sms/useGetMitigationTable.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "@/lib/axios";
+import {  MitigationTable } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchMitigationTable = async (): Promise<MitigationTable[]> => {
+  const { data } = await axiosInstance.get("/transmandu/analysis");
+  return data;
+};
+
+export const useGetMitigationTable = () => {
+  return useQuery<MitigationTable[]>({
+    queryKey: ["analysis"],
+    queryFn: fetchMitigationTable,
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+};
