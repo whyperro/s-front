065853dcbfaa2b0a3073d7459b436dc36d625392
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-19 16:48:02 -0400

    Formulario para crear plan de mitigacion

---
+++ components/forms/CreateMitigationPlanForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { CalendarIcon } from "lucide-react";
+import { es } from "date-fns/locale";
+
+import { useState } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
+import { format, min } from "date-fns";
+
+import { Separator } from "@radix-ui/react-select";
+import { useCreateMitigationPlan } from "@/actions/sms/planes_de_mitigation/actions";
+
+const FormSchema = z.object({
+  description: z.string().min(5),
+  responsible: z.string().min(5),
+  start_date: z
+    .date()
+    .refine((val) => !isNaN(val.getTime()), { message: "Invalid Date" }),
+});
+
+type FormSchemaType = z.infer<typeof FormSchema>;
+
+interface FormProps {
+  onClose: () => void;
+  id: number;
+}
+
+export default function CreateMitigationPlanForm({ onClose ,id}: FormProps) {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {},
+  });
+
+  const {createMitigationPlan} = useCreateMitigationPlan();
+
+  const onSubmit = async (data: FormSchemaType) => {
+    await createMitigationPlan.mutateAsync({
+      ...data,
+      danger_identification_id: id,
+    });
+    console.log(data);
+    onClose();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col space-y-3"
+      >
+        <FormLabel className="text-lg text-center m-2">
+          Plan de Mitigacion
+        </FormLabel>
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Descripcion de la medida</FormLabel>
+              <FormControl>
+                <Input placeholder="Descripcion de la medida" {...field} />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="responsible"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Responsable</FormLabel>
+              <FormControl>
+                <Input placeholder="Quien implementara la medida" {...field} />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="start_date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col mt-2.5">
+              <FormLabel>Fecha estimada de ejecución</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP", {
+                          locale: es,
+                        })
+                      ) : (
+                        <span>Seleccione una fecha...</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                    locale={es}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex justify-between items-center gap-x-4">
+          <Separator className="flex-1" />
+          <p className="text-muted-foreground">SIGEAC</p>
+          <Separator className="flex-1" />
+        </div>
+        <Button>Enviar reporte</Button>
+      </form>
+    </Form>
+  );
+}
