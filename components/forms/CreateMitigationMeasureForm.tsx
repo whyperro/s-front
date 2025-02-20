58d81338600685deb6b421e9f4a8325e3a565e4b
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-20 16:45:02 -0400

    modificacion de los datos de creacion de medida de mitigacion

--- components/forms/CreateMitigationMeasureForm.tsx
+++ components/forms/CreateMitigationMeasureForm.tsx
@@ -1,4 +1,4 @@
- "use client";
+"use client";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -29,11 +29,12 @@ import { cn } from "@/lib/utils";
 import { format, min } from "date-fns";
 
 import { Separator } from "@radix-ui/react-select";
+import { useCreateMitigationMeasure } from "@/actions/sms/medida_de_mitigacion/actions";
 
 const FormSchema = z.object({
   description: z.string().min(5),
-  monitoring_officer: z.string().min(5),
-  implementation_officer: z.string().min(5),
+  implementation_supervisor: z.string().min(5),
+  implementation_responsible: z.string().min(5),
   estimated_date: z
     .date()
     .refine((val) => !isNaN(val.getTime()), { message: "Invalid Date" }),
@@ -46,20 +47,28 @@ type FormSchemaType = z.infer<typeof FormSchema>;
 
 interface FormProps {
   onClose: () => void;
+  id: number;
 }
 
-export default function VoluntaryReportForm({onClose}: FormProps) {
+export default function CreateMitigationMeasureForm({
+  onClose,
+  id,
+}: FormProps) {
   const form = useForm<FormSchemaType>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      description: "",
-      monitoring_officer: "",
-      implementation_officer: "",
+      estimated_date: new Date(),
+      execution_date: new Date(),
     },
   });
 
-  const onSubmit = (data: FormSchemaType) => {
-    console.log(data);
+  const { createMitigationMeasure } = useCreateMitigationMeasure();
+
+  const onSubmit = async (data: FormSchemaType) => {
+    await createMitigationMeasure.mutateAsync({
+      ...data,
+      mitigation_plan_id: id,
+    });
     onClose();
   };
 
@@ -70,7 +79,7 @@ export default function VoluntaryReportForm({onClose}: FormProps) {
         className="flex flex-col space-y-3"
       >
         <FormLabel className="text-lg text-center m-2">
-          Medida a implementar
+          Medida a Implementar
         </FormLabel>
 
         <FormField
@@ -86,130 +95,139 @@ export default function VoluntaryReportForm({onClose}: FormProps) {
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="implementation_officer"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Encargado de la implementación</FormLabel>
-              <FormControl>
-                <Input placeholder="Quien implementara la medida" {...field} />
-              </FormControl>
-              <FormMessage className="text-xs" />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="monitoring_officer"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Encargado del seguimiento</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Quien se encarga de supervisar"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage className="text-xs" />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="estimated_date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col mt-2.5">
-              <FormLabel>Fecha estimada de ejecución</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP", {
-                          locale: es,
-                        })
-                      ) : (
-                        <span>Seleccione una fecha...</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                    locale={es}
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
 
-        <FormField
-          control={form.control}
-          name="execution_date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col mt-2.5">
-              <FormLabel>Fecha de ejecución</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP", {
-                          locale: es,
-                        })
-                      ) : (
-                        <span>Seleccione una fecha...</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                    locale={es}
+        <div className="flex justify-center items-center gap-2">
+          <FormField
+            control={form.control}
+            name="implementation_responsible"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Encargado de la implementación</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Quien implementara la medida"
+                    {...field}
                   />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="implementation_supervisor"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Encargado del seguimiento</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Quien se encarga de supervisar"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex justify-center items-center gap-2">
+          <FormField
+            control={form.control}
+            name="estimated_date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col mt-2.5 w-full">
+                <FormLabel>Fecha estimada de ejecución</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP", {
+                            locale: es,
+                          })
+                        ) : (
+                          <span>Seleccione una fecha...</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                      locale={es}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="execution_date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col mt-2.5 w-full">
+                <FormLabel>Fecha de ejecución</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP", {
+                            locale: es,
+                          })
+                        ) : (
+                          <span>Seleccione una fecha...</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                      locale={es}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <div className="flex justify-between items-center gap-x-4">
           <Separator className="flex-1" />
           <p className="text-muted-foreground">SIGEAC</p>
           <Separator className="flex-1" />
         </div>
-        <Button>Enviar reporte</Button>
+        <Button>Enviar</Button>
       </form>
     </Form>
   );
