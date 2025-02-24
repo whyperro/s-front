8fefe4f86159593a904295ec310f65e607d3919e
aaguinagalde <aaguinagalde@transmandu.com>
2025-02-24 16:46:13 -0400

    tabla para planes de mitigacion

---
+++ app/(companies)/transmandu/sms/planes_de_mitigacion/[plan_id]/medidas/[medida_id]/controles_de_seguimiento/columns.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { DataTableColumnHeader } from "@/components/tables/DataTableHeader";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import Link from "next/link";
+import { FollowUpControl, MitigationMeasure, MitigationTable } from "@/types";
+import MitigationPlanPage from "./page";
+import MitigationTableDropdownActions from "@/components/misc/MitigationTableDropdownActions";
+import { useState } from "react";
+import MitigationMeasureList from "@/components/misc/MitigationMeasureList";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+
+export const columns: ColumnDef<FollowUpControl>[] = [
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader filter column={column} title="Descripcion" />
+    ),
+    meta: { title: "Descripcion" },
+    cell: ({ row }) => {
+      return (
+        <div className="flex justify-center">{row.original.description}</div>
+      );
+    },
+  },
+  {
+    accessorKey: "date",
+    header: ({ column }) => (
+      <DataTableColumnHeader filter column={column} title="Fecha del Control" />
+    ),
+    meta: { title: "Fecha de Control" },
+    cell: ({ row }) => {
+      return (
+        <p className="font-medium text-center">
+          {format(row.original.date, "PPP", {
+            locale: es,
+          })}
+        </p>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const MitigationTable = row.original;
+      return (
+        <p></p>
+        // <MitigationTableDropdownActions mitigationTable={MitigationTable} />
+      );
+    },
+  },
+];
+
+/*
+ {followUpControls ? (
+              <Dialog>
+                <DialogTrigger>Controles de Seguimineto</DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogDescription>
+                      Controles de Seguimineto de las Medidas
+                    </DialogDescription>
+                  </DialogHeader>
+
+                  <ul>
+                    {followUpControls.map((control, index) => (
+                      <li key={control.id}>
+                        {++index} ) {control.description}
+                      </li>
+                    ))}
+                  </ul>
+
+                  <div className=" flex justify-end">
+                    <Button className="w-1/3">Ver mas</Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            ) : (
+              <p>Sin Controles Disponibles</p>
+            )}
+*/
---
+++ app/(companies)/transmandu/sms/planes_de_mitigacion/[plan_id]/medidas/[medida_id]/controles_de_seguimiento/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { ContentLayout } from "@/components/layout/ContentLayout";
+
+import { Loader2 } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+import { useParams } from "next/navigation";
+import { useGetMeasureFollowUpControl } from "@/hooks/sms/useGetMeasureFollowUpControl";
+
+type Params = {
+  plan_id: string;
+  medida_id: string; 
+}
+
+const FollowUpControlPage = () => {
+
+  const { plan_id, medida_id } = useParams<Params>();
+
+  console.log(plan_id, medida_id);
+  const {
+    data: mitigationControl,
+    isLoading,
+    isError,
+  } = useGetMeasureFollowUpControl(plan_id,medida_id);
+
+
+  return (
+    <ContentLayout title="Medidas de Mitigacion">
+      <div className="flex flex-col gap-y-2">
+        {isLoading && (
+          <div className="flex w-full h-full justify-center items-center">
+            <Loader2 className="size-24 animate-spin mt-48" />
+          </div>
+        )}
+        {mitigationControl && (
+          <DataTable columns={columns} data={mitigationControl} />
+        )}
+        {isError && (
+          <p className="text-sm text-muted-foreground">
+            Ha ocurrido un error al cargar las medidas de mitigacion...
+          </p>
+        )}
+      </div>
+    </ContentLayout>
+  );
+};
+
+export default FollowUpControlPage;
