bde617de1e9467a03152d285659534aec25bf990
Benjamin Ortiz <bortiz@transmandu.com>
2025-02-28 13:49:47 -0400

    Action daily

--- actions/desarrollo/actions.ts
+++ actions/desarrollo/actions.ts
@@ -2,31 +2,32 @@ import axiosInstance from "@/lib/axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
-interface CreateFormSchema {
-    date: string;
+interface CreateActivitySchema {
+    description: string;
+    manual_start_time?: boolean;
 }
 
-export const useCreateDailyReport = () => {
+export const useCreateActivity = () => {
     const queryClient = useQueryClient();
 
     const createMutation = useMutation({
-        mutationFn: async (data: CreateFormSchema) => {
-            await axiosInstance.post("/api/daily-report", data);
+        mutationFn: async (data: CreateActivitySchema) => {
+            await axiosInstance.post("/api/transmandu/activities", data);
         },
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["daily-report"] });
-            toast.success("¡Reporte agregado!", {
-                description: "El reporte ha sido creado correctamente."
+            queryClient.invalidateQueries({ queryKey: ["activities"] });
+            toast.success("¡Actividad creada!", {
+                description: "La actividad se ha registrado correctamente."
             });
         },
         onError: () => {
             toast.error("Oops!", {
-                description: "Hubo un error al agregar el reporte."
+                description: "Hubo un error al crear la actividad."
             });
         }
     });
 
     return {
-        createDailyReport: createMutation,
+        createActivity: createMutation,
     };
 };
