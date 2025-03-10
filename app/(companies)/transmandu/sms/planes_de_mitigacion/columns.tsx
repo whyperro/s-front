1ba73b18fbcff704adb7f2e12acd37dc1db0156f
aaguinagalde <aaguinagalde@transmandu.com>
2025-03-10 16:47:58 -0400

    dando estilo y creando la funcion para saber el indice riesgo de acuerdo al color

--- app/(companies)/transmandu/sms/planes_de_mitigacion/columns.tsx
+++ app/(companies)/transmandu/sms/planes_de_mitigacion/columns.tsx
@@ -18,6 +18,32 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+function getResult(index: string) {
+  const INTOLERABLE: string[] = ["5A", "5B", "5C", "4A", "4B", "3A"];
+  const TOLERABLE: string[] = [
+    "5D",
+    "5E",
+    "4C",
+    "4D",
+    "4E",
+    "3B",
+    "3C",
+    "3D",
+    "2A",
+    "2B",
+    "2C",
+  ];
+  const ACCEPTABLE: string[] = ["3E", "2D", "2E", "1A", "1B", "1C", "1D", "1E"];
+
+  if (INTOLERABLE.includes(index)) {
+    return "INTOLERABLE";
+  } else if (TOLERABLE.includes(index)) {
+    return "TOLERABLE";
+  } else if (ACCEPTABLE.includes(index)) {
+    return "ACEPTABLE";
+  }
+}
+
 export const columns: ColumnDef<MitigationTable>[] = [
   {
     accessorKey: "analysis",
@@ -26,41 +52,6 @@ export const columns: ColumnDef<MitigationTable>[] = [
     ),
     meta: { title: "Analisis" },
     cell: ({ row }) => {
-      function getResult(index: string) {
-        const INTOLERABLE: string[] = ["5A", "5B", "5C", "4A", "4B", "3A"];
-        const TOLERABLE: string[] = [
-          "5D",
-          "5E",
-          "4C",
-          "4D",
-          "4E",
-          "3B",
-          "3C",
-          "3D",
-          "2A",
-          "2B",
-          "2C",
-        ];
-        const ACCEPTABLE: string[] = [
-          "3E",
-          "2D",
-          "2E",
-          "1A",
-          "1B",
-          "1C",
-          "1D",
-          "1E",
-        ];
-
-        if (INTOLERABLE.includes(index)) {
-          return "INTOLERABLE";
-        } else if (TOLERABLE.includes(index)) {
-          return "TOLERABLE";
-        } else if (ACCEPTABLE.includes(index)) {
-          return "ACEPTABLE";
-        }
-      }
-
       return (
         <div className="flex flex-col justify-center text-center">
           {row.original.analysis ? (
@@ -114,7 +105,7 @@ export const columns: ColumnDef<MitigationTable>[] = [
     cell: ({ row }) => {
       return (
         <div className="flex justify-center">
-          {row.original.mitigation_plan?.id}
+          {row.original.mitigation_plan?.id ?? "N/A"}
         </div>
       );
     },
@@ -149,32 +140,42 @@ export const columns: ColumnDef<MitigationTable>[] = [
           <div className="flex flex-col justify-center">
             {measures ? (
               <Dialog>
-                <DialogTrigger className="font-semibold">Medidas</DialogTrigger>
+                <DialogTrigger className="flex justify-center items-center rounded-full h-10 text-center bg-blue-400 font-semibold">
+                  Medidas
+                </DialogTrigger>
                 <DialogContent>
                   <DialogHeader>
                     <DialogDescription className="font-semibold text-black">
-                      Medidas de Mitigacion Asociadas al Plan
+                      Medidas de Mitigacion
                     </DialogDescription>
                   </DialogHeader>
 
                   <div className="border rounded-lg p-4 shadow-md">
-                    <ul>
-                      {measures.map((measure, index) => (
-                        <li key={measure.id} className="mb-2">
-                          <span className="font-semibold"> {++index} ) </span>
-                          <span>{measure.description}</span>
-                        </li>
-                      ))}
-                    </ul>
+                    {measures.length > 0 ? (
+                      <ul>
+                        {measures.map((measure, index) => (
+                          <li key={measure.id} className="mb-2">
+                            <span className="font-semibold"> {++index} ) </span>
+                            <span>{measure.description}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <p className="text-left">
+                        No hay medidas asociadas a este plan de mitigacion
+                      </p>
+                    )}
                   </div>
 
-                  <Link
-                    href={`/transmandu/sms/planes_de_mitigacion/${planId}/medidas`}
-                  >
-                    <div className="flex justify-end mt-4">
-                      <Button className="w-1/3">Ver mas</Button>
-                    </div>
-                  </Link>
+                  {measures.length > 0 ? (
+                    <Link
+                      href={`/transmandu/sms/planes_de_mitigacion/${planId}/medidas`}
+                    >
+                      <div className="flex justify-end mt-4">
+                        <Button className="w-1/3">Ver mas</Button>
+                      </div>
+                    </Link>
+                  ) : null}
                 </DialogContent>
               </Dialog>
             ) : (
@@ -189,21 +190,46 @@ export const columns: ColumnDef<MitigationTable>[] = [
     // Debo agregar N/A no aplica, en caso de que esten vacios los campos .
     accessorKey: "mitigation_plan",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Analisis-Post Mitigacion" />
+      <DataTableColumnHeader column={column} title="Post-Mitigacion" />
     ),
     cell: ({ row }) => {
-      const mitigation_analysis = row.original.mitigation_plan?.analysis;
+      const mitigation_analyses = row.original.mitigation_plan?.analysis;
       return (
-        <div className="font-medium text-center">
-          <div className="flex flex-col justify-center">
-            <hr />
-            <p>Probabilidad: {mitigation_analysis?.probability ?? "N/A"}</p>
-            <hr />
-            <p>Severidad: {mitigation_analysis?.severity ?? "N/A"}</p>
-            <hr />
-            <p>Resultado: {mitigation_analysis?.result ?? "N/A"}</p>
-            <hr />
-          </div>
+        <div className="flex flex-col justify-center text-center">
+          {mitigation_analyses ? (
+            <>
+              <hr />
+              <p>Probabilidad: {mitigation_analyses.probability}</p>
+              <hr />
+              <p>Severidad: {mitigation_analyses.severity}</p>
+              <hr />
+              <p>Resultado: {mitigation_analyses.result}</p>
+              {(() => {
+                const riskIndex = getResult(mitigation_analyses?.result);
+                if (riskIndex === "TOLERABLE") {
+                  return (
+                    <div className="bg-yellow-400 p-4 rounded-lg">
+                      <p className="text-white">TOLERABLE</p>
+                    </div>
+                  );
+                } else if (riskIndex === "INTOLERABLE") {
+                  return (
+                    <div className="bg-red-600 p-4 rounded-lg">
+                      <p className="text-white">INTOLERABLE</p>
+                    </div>
+                  );
+                } else if (riskIndex === "ACEPTABLE") {
+                  return (
+                    <div className="bg-green-500 p-4 rounded-lg">
+                      <p className="text-white">ACEPTABLE</p>
+                    </div>
+                  );
+                } else {
+                  return null; // O un componente por defecto si lo deseas
+                }
+              })()}
+            </>
+          ) :  <p className="text-center">N/A</p>}
         </div>
       );
     },
