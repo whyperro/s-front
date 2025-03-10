3756590e897a51cd7a46a52ec5f89de085ac406c
aaguinagalde <aaguinagalde@transmandu.com>
2025-03-10 16:48:33 -0400

    reportes contadors por area dado un rango de fechas

--- hooks/sms/useGetVoluntaryReportsCountedByArea.ts
+++ hooks/sms/useGetVoluntaryReportsCountedByArea.ts
@@ -1,18 +1,24 @@
 import axiosInstance from "@/lib/axios";
-import { ReportsByArea } from "@/types";
+import { pieChartData, ReportsByArea } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
-const fetcVoluntaryReportsCountedByArea = async (from: string, to: string) => {
+const fetcVoluntaryReportsCountedByArea = async (
+  from: string,
+  to: string
+): Promise<pieChartData[]> => {
   const { data } = await axiosInstance.get(
     `transmandu/voluntary-reports/counted-by-area?from=${from}&to=${to}`
   );
   return data;
 };
 
-export const useGetVoluntaryReportsCountedByArea= (from: string, to: string) => {
-  return useQuery<ReportsByArea[]>({
+export const useGetVoluntaryReportsCountedByArea = (
+  from: string,
+  to: string
+) => {
+  return useQuery<pieChartData[]>({
     queryKey: ["voluntary-reports/counted-by-area/from=${from}&to=${to}"], // Incluye el ID en la clave de la query
-    queryFn: () => fetcVoluntaryReportsCountedByArea(from,to), // Pasa el ID a la función fetchUser
+    queryFn: () => fetcVoluntaryReportsCountedByArea(from, to), // Pasa el ID a la función fetchUser
     staleTime: 1000 * 60 * 5, // 5 minutos
   });
 };
