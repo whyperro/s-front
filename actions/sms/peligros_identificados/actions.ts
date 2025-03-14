8f7449e0c4069654bc6dde90cd5a1768d92bf2cc
alvaro <aaguinagalde@transmandu.com>
2025-03-14 00:55:46 -0400

    tipado para actualizar danger identification

--- actions/sms/peligros_identificados/actions.ts
+++ actions/sms/peligros_identificados/actions.ts
@@ -1,5 +1,6 @@
 import axiosInstance from "@/lib/axios";
 import {
+  Analysis,
   ComponentArticle,
   ConsumableArticle,
   DispatchRequest,
@@ -10,7 +11,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 interface DangerIdentificationData {
-  id: string | number;
+  id: string | number; // Este id es el del reporte al cual sera asignado la identificacion (CREO)
   danger: string;
   danger_area: string;
   description: string;
@@ -18,6 +19,18 @@ interface DangerIdentificationData {
   consequence_to_evaluate: string;
   root_cause_analysis: string;
   information_source_id: string;
+}
+
+interface UpdateDangerIdentification {
+  id: number;
+  danger: string;
+  danger_area: string;
+  danger_type: string;
+  description: string;
+  possible_consequences: string;
+  consequence_to_evaluate: string;
+  root_cause_analysis: string;
+  information_source_id: number | string;
 }
 
 export const useCreateDangerIdentification = () => {
@@ -59,7 +72,9 @@ export const useDeleteDangerIdentification = () => {
 
   const deleteMutation = useMutation({
     mutationFn: async (id: number | string) => {
-      await axiosInstance.delete(`/transmandu/sms/danger-identifications/${id}`);
+      await axiosInstance.delete(
+        `/transmandu/sms/danger-identifications/${id}`
+      );
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["danger-identifications"] });
@@ -76,5 +91,61 @@ export const useDeleteDangerIdentification = () => {
 
   return {
     deleteDangerIdentification: deleteMutation,
+  };
+};
+
+export const useDeleteVoluntaryReport = () => {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationKey: ["voluntary-reports"],
+    mutationFn: async (id: number | string) => {
+      await axiosInstance.delete(`/transmandu/sms/voluntary-reports/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["danger-identifications"] });
+      queryClient.invalidateQueries({ queryKey: ["voluntary-reports"] });
+      toast.success("¡Eliminado!", {
+        description: `¡El reporte ha sido eliminada correctamente!`,
+      });
+    },
+    onError: (e) => {
+      toast.error("Oops!", {
+        description: "¡Hubo un error al eliminar el reporte!",
+      });
+    },
+  });
+
+  return {
+    deleteVoluntaryReport: deleteMutation,
+  };
+};
+
+export const useUpdateDangerIdentification = () => {
+  const queryClient = useQueryClient();
+
+  const updateDangerIdentificationtMutation = useMutation({
+    mutationKey: ["danger-identifications"],
+    mutationFn: async (data: UpdateDangerIdentification) => {
+      await axiosInstance.patch(
+        `/transmandu/sms/danger-identifications/${data.id}`,
+        data
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["danger-identifications"] });
+      toast.success("¡Actualizado!", {
+        description: `La identificacion de peligro ha sido actualizada correctamente.`,
+      });
+    },
+    onError: (error) => {
+      toast.error("Oops!", {
+        description: "No se pudo actualizar la identificacion de peligro...",
+      });
+      console.log(error);
+    },
+  });
+  return {
+    updateDangerIdentification: updateDangerIdentificationtMutation,
   };
 };
