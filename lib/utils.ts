8af04904dcc25f4315fd681941de3c8f8ad09855
aaguinagalde <aaguinagalde@transmandu.com>
2025-03-25 16:23:56 -0400

    funcion para formartear en español y agregar un dia mas

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { format, subDays } from "date-fns";
-import { twMerge } from "tailwind-merge"
-import {es} from "date-fns/locale"
+import { type ClassValue, clsx } from "clsx";
+import { addDays, format, subDays } from "date-fns";
+import { twMerge } from "tailwind-merge";
+import { es } from "date-fns/locale";
 
 interface Period {
-  from: string | Date | undefined
-  to: string | Date | undefined
+  from: string | Date | undefined;
+  to: string | Date | undefined;
 }
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const generateSlug = (name: string) => {
   return name
     .toLowerCase()
-    .replace(/\s+/g, '-') // Reemplazar espacios con "-"
-    .replace(/[^\w-]/g, ''); // Remover caracteres especiales excepto "-"
+    .replace(/\s+/g, "-") // Reemplazar espacios con "-"
+    .replace(/[^\w-]/g, ""); // Remover caracteres especiales excepto "-"
 };
 
 export function formatDateRange(period?: Period) {
@@ -24,13 +24,28 @@ export function formatDateRange(period?: Period) {
   const defaultFrom = subDays(defaultTo, 30);
   const locale = es;
 
-  if(!period?.from) {
-    return `${format(defaultFrom, "LLL dd", {locale})} - ${format(defaultTo, "LLL dd, y", {locale})}`
+  if (!period?.from) {
+    return `${format(defaultFrom, "LLL dd", { locale })} - ${format(
+      defaultTo,
+      "LLL dd, y",
+      { locale }
+    )}`;
   }
 
-  if(period.to) {
-    return `${format(period.from, "LLL dd")} - ${format(period.to, "LLL dd, y", {locale})}`
+  if (period.to) {
+    return `${format(period.from, "LLL dd")} - ${format(
+      period.to,
+      "LLL dd, y",
+      { locale }
+    )}`;
   }
 
-  return format(period.from, "LLL dd, y", {locale});
+  return format(period.from, "LLL dd, y", { locale });
+}
+
+export function dateFormat(date: string | Date, DateFormat: string) {
+  const newDate = addDays(new Date(date), 1);
+  return format(newDate, DateFormat, {
+    locale: es,
+  });
 }
