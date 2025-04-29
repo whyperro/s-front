66bf2b383d1da88583e5a69b966c2f20a495bd23
aaguinagalde <aaguinagalde@transmandu.com>
2025-04-29 16:52:10 -0400

    agregando imagen

--- hooks/sms/useGetVoluntaryReportById.ts
+++ hooks/sms/useGetVoluntaryReportById.ts
@@ -1,5 +1,5 @@
 import axiosInstance from "@/lib/axios";
-import { User, VoluntaryReport } from "@/types";
+import { DangerIdentification, User, VoluntaryReport } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 const fetcVoluntaryReportById = async (id: string | number) => {
@@ -9,8 +9,28 @@ const fetcVoluntaryReportById = async (id: string | number) => {
   return data;
 };
 
+export type GetVoluntaryReport = {
+  id: number;
+  report_number: string;
+  report_date: Date;
+  identification_date: Date;
+  danger_location: string;
+  danger_area: string;
+  description: string;
+  airport_location: string;
+  possible_consequences: string;
+  danger_identification_id: number;
+  danger_identification: DangerIdentification;
+  status: string;
+  reporter_name?: string;
+  reporter_last_name?: string;
+  reporter_phone?: string;
+  reporter_email?: string;
+  image?: string;
+};
+
 export const useGetVoluntaryReportById = (id: string | number) => {
-  return useQuery<VoluntaryReport>({
+  return useQuery<GetVoluntaryReport>({
     queryKey: ["voluntary-report", id], // Incluye el ID en la clave de la query
     queryFn: () => fetcVoluntaryReportById(id), // Pasa el ID a la función fetchUser
     staleTime: 1000 * 60 * 5, // 5 minutos
